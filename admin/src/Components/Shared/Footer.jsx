41a210fe40affc3bb19a753765abcedd6a9b3753
shizzuuuuu-sudo <shizzuuuuu@gmail.com>
2025-11-05 15:03:14 +0530

    changes

--- admin/src/Components/Shared/Footer.jsx
+++ admin/src/Components/Shared/Footer.jsx
@@ -7,18 +7,18 @@ const Footer = () => {
       <div className="container-fluid">
         <div className="row">
           <div className="col-12 text-center">
-            {new Date().getFullYear()} &copy; Larkon. Crafted by{" "}
+            {new Date().getFullYear()} &copy; Modman. Crafted by{" "}
             <iconify-icon
               icon="iconamoon:heart-duotone"
               class="fs-18 align-middle text-danger"
             ></iconify-icon>{" "}
             <a
-              href="https://1.envato.market/techzaa"
+              href=""
               className="fw-bold footer-text"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Techzaa
+              TAT
             </a>
           </div>
         </div>
